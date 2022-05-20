2a729a9a6a2155009a606631a8f176ff6f17a97c
sagarjain0456 <72862757+sagarjain0456@users.noreply.github.com>
2022-05-21 00:37:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,19 +18,7 @@
 // }
 
 
-function myFunction() {
 
-
-document.getElementById('navitems').style.left = '0' ;
-
-document.getElementById('navitems').style.transition = 'all 0.3s ease';
-
-document.getElementById("closehide").style.display = 'inline-block';
-document.getElementById("closehide").style.transition = '3s';
-
-
-
-}
 
 
 
